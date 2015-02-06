af8fa526f43197920b42b3428c1d4674207d10c5
Shamsundar Dileep <dileepfrog@gmail.com>
2015-02-05 18:55:38 -0800

    more tweaks

--- SLPagingView/SLPagingViewController.h
+++ SLPagingView/SLPagingViewController.h
@@ -212,7 +212,7 @@ typedef void(^SLPagingViewDidChanged)(NSInteger currentPage);
 /*
  * Internal method only to be used from subclass initializers
  */
--(void) initCrucialObjects:(UIColor *)background showPageControl:(BOOL) showPageControl;
+-(void) initCrucialObjects:(UIColor *)background textColor: (UIColor *) textColor showPageControl:(BOOL);
 
 @end
 
